f46712e8d3dbaf858dc9a75e484a319798364736
Zhang Qin <zhang.ethan3@gmail.com>
2016-01-24 14:11:34 +0800

    the thread function can take arguments

--- lua/lproc.c
+++ lua/lproc.c
@@ -31,25 +31,68 @@ int ll_lua_Writer(lua_State* ls, const void* p, size_t sz, void* ud) {
 	return 0;
 }
 
+static void transfer_args(lua_State *dst, lua_State *src) {
+  int n = lua_gettop(src);
+  int i;
+  for(i = 2; i <= n; i++) {
+    int t = lua_type(src, i);
+    switch(t) {
+      case LUA_TSTRING:
+        lua_pushstring(dst, lua_tostring(src, i));
+        break;
+
+      case LUA_TBOOLEAN:
+        lua_pushboolean(dst, lua_toboolean(src, i));
+        break;
+
+      case LUA_TNUMBER:
+        lua_pushnumber(dst, lua_tonumber(src, i));
+        break;
+
+      default:
+        luaL_error(src, "unable to src type");
+        break;
+    }
+  }
+  // remove the thread function's args from src to leave the function on the stack top
+  lua_pop(src, n-1);
+}
+
 lua_State * ll_newstate(lua_State *L) {
 	if (lua_isfunction(L, 1) == 0) {
 		luaL_error(L, "It should be a lua function");
 	}
-
-	dump_func func = {0L, 0, 0};
-	if (lua_dump(L, ll_lua_Writer, &func, 0) != 0) {
-		luaL_error(L, "dump the lua function failed");
-	}
-	lua_pop(L, -1);
 
 	lua_State *L1 = luaL_newstate();
 	if (L1 == NULL) {
 		luaL_error(L, "unable to create new state");
 	}
 
+	int n = lua_gettop(L);
+	if (n>1) {
+		// reserve an empty slot for the thread function
+		lua_settop(L1, 1);
+		transfer_args(L1, L);
+	}
+
+	dump_func func = {0L, 0, 0};
+	if (lua_dump(L, ll_lua_Writer, &func, 0) != 0) {
+		luaL_error(L, "dump the lua thread function failed");
+	}
+	// pop the thread function form L because lua_dump does not pop it
+	lua_pop(L, 1);
+
 	if (luaL_loadbuffer(L1, func.data, func.len, "lproc_func") != 0) {
 		luaL_error(L, "error starting thread: %s", lua_tostring(L1, -1));
 	}
+
+	// Move the function at top to index 1
+	if (n>1) {
+		lua_replace(L1, 1);
+	}
+    
+    // push the thread function's arg numbers
+	lua_pushnumber(L1, n-1);
 
 	free(func.data);
 	return L1;
@@ -57,8 +100,9 @@ lua_State * ll_newstate(lua_State *L) {
 
 void ll_thread(lua_State *L) {
 	luaL_openlibs(L);
-
-	if(lua_pcall(L, 0, 0, 0) != 0) {
+    int nargs = lua_tointeger(L, -1);
+    lua_pop(L, 1);
+	if(lua_pcall(L, nargs, 0, 0) != 0) {
 		fprintf(stderr, "thread error: %s", lua_tostring(L, -1));
 	}
 
--- lua/lproc.h
+++ lua/lproc.h
@@ -1,4 +1,4 @@
-// All functions composited with lua c api
+// Support lua multi-thread backed by goroutine of golang
 
 lua_State * ll_newstate(lua_State *L);
 void ll_thread(lua_State *L);
