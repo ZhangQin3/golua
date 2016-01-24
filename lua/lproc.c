3356a704e167b51c3e083ce017f5687a027cbf4d
Zhang Qin <zhang.ethan3@gmail.com>
2016-01-24 10:37:14 +0800

    Modify some comments.

--- lua/lproc.c
+++ lua/lproc.c
@@ -1,4 +1,4 @@
-// All functions composited with lua c api
+// Support lua multi-thread backed by goroutine of golang
 
 #include <lua.h>
 #include <lauxlib.h>
@@ -56,35 +56,11 @@ lua_State * ll_newstate(lua_State *L) {
 }
 
 void ll_thread(lua_State *L) {
-	// lua_State *L = (lua_State *)arg;
-	// openlibs(L);
-	// luaL_requiref(L, "lproc", luaopen_lproc, 1);
 	luaL_openlibs(L);
-	// lua_pop(L, 1);
 
 	if(lua_pcall(L, 0, 0, 0) != 0) {
 		fprintf(stderr, "thread error: %s", lua_tostring(L, -1));
 	}
 
-	// pthread_cond_destroy(&getself(L)->cond);
 	lua_close(L);
 }
-
-
-
-
-// void openlibs(lua_State *L) {
-//   luaL_requiref(L, "_G", luaopen_base, 1);
-//   luaL_requiref(L, "package", luaopen_package, 1);
-//   lua_pop(L, 2);
-//   registerlib(L, "io", luaopen_io);
-//   registerlib(L, "os", luaopen_os);
-//   registerlib(L, "table", luaopen_table);
-//   registerlib(L, "string", luaopen_string);
-//   registerlib(L, "math", luaopen_math);
-//   registerlib(L, "debug", luaopen_debug);
-// }
-
-
-
-
