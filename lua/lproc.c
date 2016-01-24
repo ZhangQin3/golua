049fc94bca66d27f2723ae566d5a36e69dd08133
Zhang Qin <zhang.ethan3@gmail.com>
2016-01-24 10:17:24 +0800

    multi-thread supported by go routine

--- lua/lproc.c
+++ lua/lproc.c
@@ -5,21 +5,56 @@
 #include <lualib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "_cgo_export.h"
 
+typedef struct {
+	char *data;
+	size_t cap;
+    size_t len;
+} dump_func;
 
-lua_State * ll_newstate(lua_State *L){
-	const char *chunk = luaL_checkstring(L, 1);
+int ll_lua_Writer(lua_State* ls, const void* p, size_t sz, void* ud) {
+    dump_func* bd = (dump_func*)ud;
+
+    char* newData;
+    if (bd->len + sz > bd->cap) {
+	    newData = (char*)realloc(bd->data, bd->cap + 512);
+	}
+
+    if (newData) {
+        memcpy(newData + bd->len, p, sz);
+        bd->data = newData;
+        bd->len += sz;
+    } else {
+        free(newData);
+        return 1;
+    }
+
+    return 0;
+}
+
+lua_State * ll_newstate(lua_State *L) {
+	if (lua_isfunction(L, 1) == 0) {
+		luaL_error(L, "It should be a lua function");
+	}
+
+	dump_func func = {0L, 0, 0};
+	if (lua_dump(L, ll_lua_Writer, &func, 0) != 0) {
+		luaL_error(L, "dump the lua function failed");
+	}
+	lua_pop(L, -1);
+
 	lua_State *L1 = luaL_newstate();
-
-	if(L1 == NULL) {
+	if (L1 == NULL) {
 		luaL_error(L, "unable to create new state");
 	}
 
-	if(luaL_loadstring(L1, chunk) != 0) {
+	if (luaL_loadbuffer(L1, func.data, func.len, "lproc_func") != 0) {
 		luaL_error(L, "error starting thread: %s", lua_tostring(L1, -1));
 	}
 
+	free(func.data);
 	return L1;
 }
 
@@ -37,6 +72,9 @@ void ll_thread(lua_State *L) {
 	// pthread_cond_destroy(&getself(L)->cond);
 	lua_close(L);
 }
+
+
+
 
 // void openlibs(lua_State *L) {
 //   luaL_requiref(L, "_G", luaopen_base, 1);
